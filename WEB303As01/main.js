8dcbf8de48cd1baa4db53ed74747ff274eedba76
yashviraja <YR6@myscc.ca>
2021-09-19 06:17:23 +0530

    write code for keyup event listener

--- WEB303As01/main.js
+++ WEB303As01/main.js
@@ -7,4 +7,16 @@
 
 $(document).ready(function() {
 
+	let input = document.querySelector('fieldset');
+	input.addEventListener('keyup',calculateSalary);
+
+	function calculateSalary(event) {
+		let salary = document.querySelector("#salary").value;
+
+		let percent = document.querySelector("#percent").value;
+
+		let gadgets = salary * percent / 100 ;
+
+		document.querySelector('#spend').textContent = "$" + gadgets.toFixed(2);
+	}
 });
