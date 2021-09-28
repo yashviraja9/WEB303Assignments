8add52312a0279fcaf0af8b295b2f5dd3d671f3c
yashviraja <YR6@myscc.ca>
2021-09-28 07:29:32 +0530

    write code to load content on index page

--- WEB303As02/js/script.js
+++ WEB303As02/js/script.js
@@ -1,3 +1,20 @@
 // WEB303 Assignment 2
 // Yashvi Brijmohan Raja
 
+$(document).ready(function(){
+    $("a#vprospect").click(function(){
+        $("#solution").hide().load("prospect.html").slideDown(2000); 
+        console.log("Prospect is working!");
+
+    });
+    $("a#vconvert").click(function(){
+        $("#solution").hide().load("convert.html").slideDown(2000);  
+        console.log("Covert is working!");
+
+    });
+    
+    $("a#vretain").click(function(){
+        $("#solution").hide().load("retain.html").slideDown(2000);
+        console.log("Retain is working!");
+    });
+});
