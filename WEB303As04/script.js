16064e26ee6712dc0555dcfe28081996c7ee8e6e
yashviraja <YR6@myscc.ca>
2021-10-06 07:25:08 +0530

    Upload stater files

---
+++ WEB303As04/script.js
@@ -0,0 +1,34 @@
+/*
+    Assignment 4
+    {Your name here}
+*/
+
+$(document).ready(function(){
+    // your code here
+
+
+
+
+
+
+    // function to calculate the distance in metres between two lat/long pairs on Earth
+    // Haversine formula - https://en.wikipedia.org/wiki/Haversine_formula
+    // Aren't those cool variable names? Yah gotta love JavaScript
+    function calcDistance(lat1, lon1, lat2, lon2){
+        var toRadians = function(num) {
+            return num * Math.PI / 180;
+        }
+        var R = 6371000; // radius of Earth in metres
+        var φ1 = toRadians(lat1);
+        var φ2 = toRadians(lat2);
+        var Δφ = toRadians(lat2-lat1);
+        var Δλ = toRadians(lon2-lon1);
+
+        var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return ( R * c );
+    }
+});
+
+
